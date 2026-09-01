52505f4fc1acf99cbe4a8b847b599bf4efd3f41d
Angela Long <angelalong124@icloud.com>
2026-08-31 23:08:28 -0400

    created script for main menu and start game button goes to the game screen

---
+++ Scenes/main_menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func _on_start_game_pressed():
+	get_tree().change_scene_to_file("res://Scenes/game.tscn")
+
+
+func _on_version_notes_pressed():
+	print("Version notes pressed")
+
+
+func _on_credits_pressed():
+	print("Credits pressed")
