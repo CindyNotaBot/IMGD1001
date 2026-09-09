cdfa06e9191845b51a287e21de1b10c31d8e7362
Angela Long <angelalong124@icloud.com>
2026-09-09 16:33:02 -0400

    made version notes screen into an auto scroll screen

--- Scenes/version_notes.gd
+++ Scenes/version_notes.gd
@@ -1,14 +1,22 @@
 extends Control
 
+@onready var scroll_container: ScrollContainer = $Panel/ScrollContainer
+
+var scroll_speed: float = 25.0
+var scroll_position: float = 0.0
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+func _ready():
+	scroll_position = 0.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(delta):
+	scroll_position += scroll_speed * delta
+	scroll_container.scroll_vertical = int(scroll_position)
+	
+	if scroll_position >= scroll_container.get_v_scroll_bar().max_value - scroll_container.get_v_scroll_bar().page:
+		scroll_position = 0.0
+		scroll_container.scroll_vertical = 0
 
 # When back  button is pressed screen is changed to the main menu
 func _on_back_button_pressed():
