6a19a5b086eb7218b67f71182df8039e0e9271ec
Angela Long <angelalong124@icloud.com>
2026-09-16 16:31:58 -0400

    added base level design to test grappling hook

---
+++ Scenes/grapple_controller.gd
@@ -0,0 +1,57 @@
+extends Node2D 
+
+@export var rest_length = 2.0
+@export var stiffness = 10.0
+@export var damping= 2.0
+
+@onready var player := get_parent()
+@onready var ray := $RayCast2D
+@onready var rope := $Line2D
+
+var launched = false
+var target: Vector2 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	ray.look_at(get_global_mouse_position())
+	
+	if Input.is_action_just_pressed("grapple"):
+		launch()
+	if Input.is_action_just_pressed("grapple"):
+		retreat()
+		
+	if launched:
+		handle_grapple(delta)
+		
+func launch():
+	if ray.is_colliding(): 
+		launched = true
+		target = ray.get_collision_point()
+		rope.show()
+	
+func retreat():
+	launched = false
+	rope.hide()
+
+func handle_grapple(delta):
+	var target_direction = player.global_position.direction_to(target)
+	var target_distance = player.global_position.distance_to(target)
+	
+	var displacement = target_distance - rest_length
+	
+	var force = Vector2.ZERO
+	
+	if displacement > 0 :
+		var spring_force_magnitude = stiffness * displacement
+		var spring_force = target_direction * spring_force_magnitude
+		
+		var vel_dot = player.velocity.dot(target_direction)	
+		var damping = -damping * vel_dot * target_direction
+		
+		force = spring_force + damping
+	
+	player.velocity += force * delta
+	update_rope()
+
+func update_rope():
+	rope.set_point_position(1, to_local(target))
--- Scripts/player.tscn.gd
+++ Scripts/player.tscn.gd
@@ -1,35 +1,41 @@
 extends CharacterBody2D
 
-
-const SPEED = 130.0
+const BASE_SPEED = 100.0
+var SPEED = BASE_SPEED
 const JUMP_VELOCITY = -300.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var dash_limit = 1
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-
-func _physics_process(delta):
+func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity += get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction: -1, 0, 1
-	var direction = Input.get_axis("move left", "move right")
+		
+	# Get the input directions: -1, 0, 1
+	var direction := Input.get_axis("move_left", "move_right")
 	
-	# Flip the Sprite
+	# Flip the sprite
 	if direction > 0:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
+		
+	# Handle dash
+	if Input.is_action_just_pressed("dash"):
+		if dash_limit > 0:
+			$dashTimer.start()
+			SPEED *= 10
+			velocity.x = direction * SPEED
+			dash_limit = 0
 	
 	# Play animations
 	if is_on_floor():
+		dash_limit = 1
 		if direction == 0:
 			animated_sprite.play("idle")
 		else:
@@ -44,3 +50,6 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func _on_dash_timer_timeout() -> void:
+	SPEED = BASE_SPEED
